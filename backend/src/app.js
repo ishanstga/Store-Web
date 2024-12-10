9f35c6245f0a1482f1c665466a4b37b226a6a27b
ishanstga <ishanstga@gmail.com>
2024-12-10 10:48:54 +0530

    Connected MongoDB and Update CRUD operations.

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,17 +1,13 @@
 import express from "express";
 import { productRouter } from "./api/product.js";
 import globalErrorHandlingMiddleware from "./api/middleware/global-error-handling-middleware.js";
+import { connectDB } from "./insfastructure/db.js";
 
 const app = express();
 app.use(express.json()); // For parsing JSON requests
 
-// app.use((req, res, next) => {
-//   console.log("Recieved a Request");
-//   console.log(req.method, req.url);
-//   next();
-// });
-
-app.use("/api/products", productRouter);
+app.use("/products", productRouter);
 app.use(globalErrorHandlingMiddleware);
 
+connectDB();
 app.listen(8000, () => console.log(`Server running on port ${8000}`));
